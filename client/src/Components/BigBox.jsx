c3709d2cd33831f3d42b1fd13fef6232832a1534
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-29 19:30:35 -0600

    styling booklet and BigBox samples

--- client/src/Components/BigBox.jsx
+++ client/src/Components/BigBox.jsx
@@ -1,3 +1,6 @@
+import lighthouse from '../assets/Photos/BigBox/lighthouse.jpg'
+import PinkBuilding from '../assets/Photos/BigBox/PinkBuilding.jpg'
+import GloomyBuilding from '../assets/Photos/BigBox/GloomyBuilding.jpg'
 export default function BigBox({}) {
 
 
@@ -6,11 +9,13 @@ export default function BigBox({}) {
 
   return (
     <>
-    <div className="bbContainer"> 
 
-    <div className="bbSquare1 square">1</div>
-    <div className="bbSquare2 square">2</div>
-    <div className="bbSquare3 square">3</div>
+    <h1>Wayfarer</h1>
+    <h1>Travel Photography</h1>
+    <div className="bbContainer"> 
+    <div className="bbSquare1 square"><img src={lighthouse}/></div>
+    <div className="bbSquare2 square"><img src={PinkBuilding}/></div>
+    <div className="bbSquare3 square"><img src={GloomyBuilding}/></div>
     <div className="bbSquare4 square">4</div> 
     <div className="bbSquare5 square">5</div>
     <div className="bbSquare6 square">6</div>
--- client/src/Components/Booklet.jsx
+++ client/src/Components/Booklet.jsx
@@ -1,3 +1,6 @@
+import sculpture1 from '../assets/Photos/Booklet/sculpture1.jpg'
+import sculpture2 from '../assets/Photos/Booklet/sculpture2.jpg'
+
 export default function Booklet({}) {
 
 
@@ -6,9 +9,11 @@ export default function Booklet({}) {
 
   return (
     <>
+    <h1>Art & Objects</h1>
     <div className="bkltContainer"> 
-    <div className="bkltSquare1 bkltSquare">1</div> 
-    <div className="bkltSquare2 bkltSquare">2</div>
+    <div className="bkltSquare1 bkltSquare"><img src={sculpture2}/></div> 
+    <div className="bkltSquare2 bkltSquare"><img src={sculpture1}/></div>
+    {/* <div className="bkltSquare2 bkltSquare">Art and Objects</div> */}
     </div>
     </>
 );
