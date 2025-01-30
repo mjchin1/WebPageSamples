cacd3186484bf9b10ba1c8d783bbcea81dacbc4b
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-29 18:41:58 -0600

    styling block frame sample

--- client/src/Components/BlockFrame.jsx
+++ client/src/Components/BlockFrame.jsx
@@ -1,13 +1,28 @@
+import elephant from '../assets/Photos/BlockFrame/elephant.jpg'
+import sculpture1 from '../assets/Photos/BlockFrame/sculpture1.jpg'
+import vases from '../assets/Photos/BlockFrame/vases.jpg'
+import sculpture2 from '../assets/Photos/BlockFrame/sculpture2.jpg'
+
 export default function BlockFrame({}) {
 
 
 
   return (
     <>
+    <h1> Little Elephant</h1>
+    <h1>Art & Objects</h1>
     <div className="bfContainer"> 
-    <div className="bfSquare1 square">1</div> 
-    <div className="bfSquare2 square">2</div>
+    <div className="bfSquare1 square"></div> 
+    <div className="bfSquare2 square"><img src={elephant}/> </div>
     </div>
+
+    <div className="bfPictureRow">
+    <div className="bfSquare3 square"> <img src={sculpture1}/>  </div>
+    <div className="bfSquare3 square"> <img src={vases}/> </div>
+    <div className="bfSquare3 square"> <img src={sculpture2}/>  </div>
+    </div>
+
+
     </>
 );
 
