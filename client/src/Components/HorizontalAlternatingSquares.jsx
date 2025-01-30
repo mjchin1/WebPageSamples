3fc16dd2d5ed726c117f52a89d75664813448a81
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-29 18:57:10 -0600

    styling Horizontal Alternating Squares sample

--- client/src/Components/HorizontalAlternatingSquares.jsx
+++ client/src/Components/HorizontalAlternatingSquares.jsx
@@ -1,3 +1,7 @@
+import SnackPlate from '../assets/Photos/HorizontalAlternatingSquares/SnackPlate.jpg'
+import tea from '../assets/Photos/HorizontalAlternatingSquares/tea.jpg'
+import cupcake from '../assets/Photos/HorizontalAlternatingSquares/cupcake.jpg'
+
 export default function HorizontalAlternatingSquares({}) {
 
 
@@ -6,10 +10,11 @@ export default function HorizontalAlternatingSquares({}) {
 
   return (
     <>
+    <h1>Millie's Tea Shop</h1>
     <div className="hasContainer"> 
-    <div className="hasSquare1 square">1</div> 
-    <div className="hasSquare2 square">2</div>
-    <div className="hasSquare3 square">3</div>
+    <div className="hasSquare1 square"><img src={SnackPlate}/></div> 
+    <div className="hasSquare2 square"><img src={tea}/></div>
+    <div className="hasSquare3 square"><img src={cupcake}/></div>
 
     </div>
     </>
