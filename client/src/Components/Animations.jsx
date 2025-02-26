44a31a18020572688b74e5e2364bbbdc91d5157d
Melissa J. Chin <melissa1jchin@gmail.com>
2025-02-26 17:45:52 -0600

    rotate animation

--- client/src/Components/Animations.jsx
+++ client/src/Components/Animations.jsx
@@ -6,6 +6,9 @@ export default function Animations() {
 
   return (
     <>
+    <div className="text"><h1 className="text1">Hello</h1> </div> <br/>
+    
+    <div className="text"><h1 className="text2">Hello</h1> </div>
    
     </>
 );
