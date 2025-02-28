55c3380e90ded01fa1c31378b4c5533757c00611
Melissa J. Chin <melissa1jchin@gmail.com>
2025-02-28 15:52:41 -0600

    adding fixed background sample file and photos

---
+++ client/src/Components/FixedBackground.jsx
@@ -0,0 +1,21 @@
+import coffee from '../assets/Photos/FixedBackground/coffee.jpg'
+import peaches from '../assets/Photos/FixedBackground/peaches.jpg'
+import oranges from '../assets/Photos/FixedBackground/oranges.jpg'
+import bowl from '../assets/Photos/FixedBackground/bowl.jpg'
+import pitcher from '../assets/Photos/FixedBackground/pitcher.jpg'
+import plant from '../assets/Photos/FixedBackground/plant.jpg'
+export default function FixedBackground() {
+
+
+
+
+
+  return (
+    <>
+    <div className="text"><img src={peaches}/></div> <br/>
+    </>
+);
+
+}
+
+
