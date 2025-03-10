3689f0a5d8aeb7702dca03e13f58ca54b8594f9f
Melissa J. Chin <melissa1jchin@gmail.com>
2025-03-10 15:53:51 -0500

    adding content to fixedBackgroundTemplate

--- client/src/Components/FixedBackground.jsx
+++ client/src/Components/FixedBackground.jsx
@@ -13,7 +13,24 @@ export default function FixedBackground() {
   return (
     <>
     <div className="backgroundPic"><img src={peaches}/></div> <br/>
-    <div className="pageContent"></div>
+    <div className="pageContent">
+    <h1>Fixed Background</h1>
+
+    <div className="fixedBackgroundButtons">
+    <button className="fbButton">Wine</button> <br/> <br/>
+    <button className="fbButton">Food</button> <br/> <br/>
+    <button className="fbButton">Reservations</button>
+    </div>
+
+    {/* <h1>Menu</h1>
+    <div className="menuCard"></div> */}
+
+    <h1>Contact</h1>
+    
+
+
+
+    </div>
     </>
 );
 
