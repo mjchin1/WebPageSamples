e910015466f3be7018266c5b5004d99f8733cd4b
Melissa J. Chin <melissa1jchin@gmail.com>
2025-03-10 16:02:22 -0500

    reformatting text

--- client/src/Components/FixedBackground.jsx
+++ client/src/Components/FixedBackground.jsx
@@ -14,7 +14,7 @@ export default function FixedBackground() {
     <>
     <div className="backgroundPic"><img src={peaches}/></div> <br/>
     <div className="pageContent">
-    <h1>Fixed Background</h1>
+    <h1 className="fbHeader">Fixed Background</h1>
 
     <div className="fixedBackgroundButtons">
     <button className="fbButton">Wine</button> <br/> <br/>
@@ -26,6 +26,9 @@ export default function FixedBackground() {
     <div className="menuCard"></div> */}
 
     <h1>Contact</h1>
+    <span className="contactInfo">Phone: 888-888-8888</span> <br/>
+    <span className="contactInfo">Address: 1600 Peach Tree Lane, Atlanta GA</span> <br/>
+    <span className="contactInfo">Email: hello@fixedbackground.com</span> <br/> <br/> <br/>
     
 
 
