297372a4853feadf8c4cc991672fa4479ea49f8b
Melissa J. Chin <melissa1jchin@gmail.com>
2025-03-11 18:15:23 -0500

    updating fixed background buttons

--- client/src/Components/FixedBackground.jsx
+++ client/src/Components/FixedBackground.jsx
@@ -24,19 +24,21 @@ export default function FixedBackground() {
     <button className="fbBackButton" onClick={navToHome}>Back</button>
     <div className="pageContent"> 
     <h1 className="fbHeader">Fixed Background</h1>
+    
     <div className="fixedBackgroundButtons">
-    <button className="fbButton">Wine</button> <br/> <br/>
-    <button className="fbButton">Food</button> <br/> <br/>
-    <button className="fbButton">Reservations</button>
+    <button className="fbButton fbButton1">Wine</button> <br/> <br/>
+    <button className="fbButton fbButton2">Food</button> <br/> <br/>
+    <button className="fbButton fbButton3">Reservations</button>
     </div>
 
     {/* <h1>Menu</h1>
     <div className="menuCard"></div> */}
-
+    <div className="contactDiv">
     <h1>Contact</h1>
     <span className="contactInfo">Phone: 888-888-8888</span> <br/>
     <span className="contactInfo">Address: 1600 Peach Tree Lane, Atlanta GA</span> <br/>
     <span className="contactInfo">Email: hello@fixedbackground.com</span> <br/> <br/> <br/>
+    </div>
     
 
 
