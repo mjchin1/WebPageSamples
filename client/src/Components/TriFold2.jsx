824651d4bf8141e9d5f51b347c5c1fea9e500a15
Melissa J. Chin <melissa1jchin@gmail.com>
2025-03-11 14:55:20 -0500

    alternating images animation sucessful

--- client/src/Components/TriFold2.jsx
+++ client/src/Components/TriFold2.jsx
@@ -1,6 +1,9 @@
 import steps from '../assets/Photos/TriFold/steps.jpg'
 import fabric from '../assets/Photos/TriFold/fabric.jpg'
 import pebbles from '../assets/Photos/TriFold/pebbles.jpg'
+import plant1 from '../assets/Photos/TriFold/plant1.jpg'
+import plant2 from '../assets/Photos/TriFold/plant2.jpg'
+import plant3 from '../assets/Photos/TriFold/plant3.jpg'
 
 export default function TriFold2({}) {
 
@@ -13,9 +16,18 @@ export default function TriFold2({}) {
 
     <h1>Trifold 2</h1>
     <div className="tf2Container"> 
-    <div className="tf2Square1 tf2Square"><img src={steps}/></div> 
-    <div className="tf2Square2 tf2Square"><img src={pebbles}/></div>
-    <div className="tf2Square3 tf2Square"><img src={fabric}/></div>
+    <div className="tf2Square1 tf2Square">
+      <img className="image1" src={steps}/>
+      <img className="bottomImage1" src={plant1}/>
+    </div> 
+    <div className="tf2Square2 tf2Square">
+      <img src={pebbles}/>
+      <img className="bottomImage2" src={plant2}/>
+      </div>
+    <div className="tf2Square3 tf2Square">
+      <img src={fabric}/>
+      <img className="bottomImage3" src={plant3}/>
+      </div>
     </div>
     </>
 );
