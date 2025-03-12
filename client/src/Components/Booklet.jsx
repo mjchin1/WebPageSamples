a5f7f76102dca5d4780f995dd3d37965ec464cc0
Melissa J. Chin <melissa1jchin@gmail.com>
2025-03-12 18:38:00 -0500

    upated nav bar formatting

--- client/src/Components/Booklet.jsx
+++ client/src/Components/Booklet.jsx
@@ -23,7 +23,7 @@ export default function Booklet({}) {
     <div className="bkltContainer"> 
     <div className="bkltSquare1 bkltSquare"><img src={sculpture2}/></div> 
     <div className="bkltSquare2 bkltSquare"><img src={sculpture1}/></div>
-    {/* <div className="bkltSquare3"> A Beautiful Way to Tell Your Brand's Story. </div> <br/> 
+    {/* <div className="bkltSquare3"> A minimal canvas that lets your work shine. </div> <br/> 
     <div className="bkltSquare4 bkltSquare"><img src={cups}/></div>  */}
 
     </div>
