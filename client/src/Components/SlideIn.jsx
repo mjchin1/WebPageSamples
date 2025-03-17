2cb0e0636dfd40d3f8c05b4560e651e7fccc53dc
Melissa J. Chin <melissa1jchin@gmail.com>
2025-03-17 11:09:27 -0500

    working on slideIn wireframes

--- client/src/Components/SlideIn.jsx
+++ client/src/Components/SlideIn.jsx
@@ -18,11 +18,34 @@ export default function SlideIn() {
   
     <button className="sldnBackButton" onClick={navToHome}>Home</button>
     <div className="sldnPageContent"> 
-    <h1 className="sldnHeader">Slide-In</h1>
-    <div className="slidingDivs">
-      <div className="sldnDiv1"></div>
-      <div className="sldnDiv2"></div>
+    
+    <div className="sldnContentBlock1">
+      <h1 className="sldnHeader">Slide-In</h1>
+      <div className="slidingDivs">
+        <div className="sldnDiv1"></div>
+        <div className="sldnDiv2"></div>
+      </div>
     </div>
+
+    <div className="sldnContentBlock2">
+    <div className="sldnTextBlock">Text</div>
+    <div className="sldnImageBlock"></div> 
+    <div className="sldnImageBlock"></div>
+    <div className="sldnTextBlock">Text</div>
+    </div>
+
+    <div className="sldnContentBlock3">
+    <div className="sldnImageBlock2"></div>
+    <div className="sldnImageBlock2"></div> 
+    <div className="sldnImageBlock2"></div>
+    </div>
+
+    <div className="sldnContentBlock4">
+    <div className="sldnImageBlock4"></div>
+    <div className="sldnTextBlock4">Text</div> 
+    </div>
+
+    <div className="footer"></div>
 
     </div>
     </>
