1dbd1c3f7dcfb1958b8bd6cb0e9ceb0819ba2170
Melissa J. Chin <melissa1jchin@gmail.com>
2025-03-17 12:33:48 -0500

    working on outline page composition

--- client/src/Components/Outline.jsx
+++ client/src/Components/Outline.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 import chicago from '../assets/Photos/Outline/chicago.jpg'
 
@@ -19,9 +19,13 @@ export default function Outline() {
   return (
     <>
   
-    <button className="outlnBackButton" onClick={navToHome}>Home</button>
+    {/* <button className="outlnBackButton" onClick={navToHome}>Home</button> */}
     <div className="outlnPageContent"> 
-    
+    {/* <div className="outlnHeader2">
+    <h1 className="outlnLogo">Outline</h1>
+    <Link to = '/'><h3 className="outlnNav">Home</h3></Link>
+    </div>
+     */}
     <div className="outlnContentBlock1">
       <div className="outln1"></div> <br/>
       <h1 className="outlnHeader">Outline</h1> <br/> <br/>
@@ -29,6 +33,12 @@ export default function Outline() {
     </div>
 
     <div className="outlnContentBlock2">
+    <div className="outlnHeader2">
+    <h1 className="outlnLogo">Outline</h1>
+    <Link to = '/'><h3 className="outlnNav">Home</h3></Link>
+    </div>
+    
+      
       <div className="outlnMainImage"><img src={chicago}/></div>
     </div>
 
