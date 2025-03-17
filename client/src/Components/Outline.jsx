d809e671124e2202cb709831a88b1acda92dec34
Melissa J. Chin <melissa1jchin@gmail.com>
2025-03-17 12:04:14 -0500

    building outline template content blocks

--- client/src/Components/Outline.jsx
+++ client/src/Components/Outline.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 
+import chicago from '../assets/Photos/Outline/chicago.jpg'
+
 
 export default function Outline() {
 
@@ -20,9 +22,15 @@ export default function Outline() {
     <button className="outlnBackButton" onClick={navToHome}>Home</button>
     <div className="outlnPageContent"> 
     
-    <div className="outln1"></div>  
-    <h1 className="outlnHeader">Outline</h1>
-    <div className="outln2"></div>
+    <div className="outlnContentBlock1">
+      <div className="outln1"></div> <br/>
+      <h1 className="outlnHeader">Outline</h1> <br/> <br/>
+      <div className="outln2"></div>
+    </div>
+
+    <div className="outlnContentBlock2">
+      <div className="outlnMainImage"><img src={chicago}/></div>
+    </div>
 
     </div>
     </>
