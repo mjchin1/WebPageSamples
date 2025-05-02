3717ad0c98fffb699ed08f21489fc6a0837b4100
Melissa J. Chin <melissa1jchin@gmail.com>
2025-05-02 14:54:21 -0500

    adding photos to BigBook

--- client/src/Components/BigBook.jsx
+++ client/src/Components/BigBook.jsx
@@ -1,5 +1,9 @@
-import sculpture1 from '../assets/Photos/Booklet/sculpture1.jpg'
-import sculpture2 from '../assets/Photos/Booklet/sculpture2.jpg'
+import stillLife from '../assets/Photos/BigBook/stillLife.jpg'
+import vestibule from '../assets/Photos/BigBook/vestibule.jpg'
+import stool1 from '../assets/Photos/BigBook/stool1.jpg'
+import greenCouch from '../assets/Photos/BigBook/greenCouch.jpg'
+import mountains from '../assets/Photos/BigBook/mountains.jpg'
+import sphereLamp from '../assets/Photos/BigBook/sphereLamp.jpg'
 import {Link} from 'react-router-dom'
 
 export default function BigBook({}) {
@@ -25,15 +29,17 @@ export default function BigBook({}) {
         <span>Gallery</span>
         </div>
       </div> 
-    <div className="bbkSquare2 bbkTopPhoto"><img src={sculpture1}/></div>
+    <div className="bbkSquare2 bbkTopPhoto"><img src={stillLife}/></div>
     {/* <div className="bbkDivider">sample text</div> */}
-    <div className="bbkBottomPhoto"><p className="bbkContactHeader"></p> <img src={sculpture1}/></div>
-    <div className="bbkSquare2 bbkTextBlock"><p className="bbkText">Visit the Shop</p></div>
-    <div className="bbkSquare2 bbkMidPhoto"><img src={sculpture1}/></div>
-    <div className="bbkSquare1 bbkMidPhoto"><img src={sculpture2}/></div> 
-    <div className="bbkSquare2 bbkTextBlock"><p className="bbkText">View the Gallery</p></div>
+    <div className="bbkBottomPhoto"><p className="bbkContactHeader"></p> <img src={vestibule}/></div>
+    <div className="bbkSquare2 bbkTextBlock"><p className="bbkText">Objects</p></div>
+    <div className="bbkSquare2 bbkMidPhoto"><img src={mountains}/></div>
+    <div className="bbkSquare1 bbkMidPhoto"><img src={sphereLamp}/></div> 
+    <div className="bbkSquare2 bbkTextBlock"><p className="bbkText">Lighting</p></div>
+    <div className="bbkSquare2 bbkTextBlock"><p className="bbkText">Furniture</p></div>
+    <div className="bbkSquare2 bbkMidPhoto"><img src={stool1}/></div>
      {/* <div className="bbkDivider">Come see us or get in touch.</div> */}
-    <div className="bbkBottomPhoto"><p className="bbkContactHeader">Contact</p> <img src={sculpture1}/></div>
+    <div className="bbkBottomPhoto"><p className="bbkContactHeader">Contact</p> <img src={greenCouch}/></div>
     {/* <div className="bbkSquare3"> A minimal canvas for your masterpieces. </div> <br/> 
     <div className="bbkSquare4 bkltSquare"><img src={cups}/></div>  */}
 
