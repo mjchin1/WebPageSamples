07052e40886625afe53ee0db90aa4418bc9b6c31
Melissa J. Chin <melissa1jchin@gmail.com>
2025-05-02 10:52:00 -0500

    adding photos to mosaic grid sample

--- client/src/Components/MosaicGrid.jsx
+++ client/src/Components/MosaicGrid.jsx
@@ -1,10 +1,10 @@
 
-import photo1 from '../assets/Photos/MosaicGrid/photo1.jpg'
-import photo2 from '../assets/Photos/MosaicGrid/photo2.jpg'
-import photo3 from '../assets/Photos/MosaicGrid/photo3.jpg'
-import photo4 from '../assets/Photos/MosaicGrid/photo4.jpg'
-import photo5 from '../assets/Photos/MosaicGrid/photo5.jpg'
-import photo6 from '../assets/Photos/MosaicGrid/photo6.jpg'
+import plate1 from '../assets/Photos/MosaicGrid/plate1.jpg'
+import plate2 from '../assets/Photos/MosaicGrid/plate2.jpg'
+import plate5 from '../assets/Photos/MosaicGrid/plate5.jpg'
+import pomegranate from '../assets/Photos/MosaicGrid/pomegranate.jpg'
+import lemon from '../assets/Photos/MosaicGrid/lemon.jpg'
+import plate4 from '../assets/Photos/MosaicGrid/plate4.jpg'
 
 import { useNavigate, Link } from 'react-router-dom'
 
@@ -23,12 +23,12 @@ export default function MosaicGrid({}) {
     </div>
 
     <div className="mosaicGridContainer"> 
-    <div className="mgSquare1 mgSquare"><img src={photo1}/></div>
-    <div className="mgSquare2 mgSquare"><img src={photo2}/></div>
-    <div className="mgSquare3 mgSquare"><img src={photo3}/></div>
-    <div className="mgSquare4 mgSquare"><img src={photo4}/></div> 
-    <div className="mgSquare5 mgSquare"><img src={photo5}/></div>
-    <div className="mgSquare6 mgSquare"><img src={photo6}/></div>
+    <div className="mgSquare1 mgSquare"><img src={plate1}/></div>
+    <div className="mgSquare2 mgSquare"><img src={plate4}/></div>
+    <div className="mgSquare3 mgSquare"><img src={pomegranate}/></div>
+    <div className="mgSquare4 mgSquare"><img src={lemon}/></div> 
+    <div className="mgSquare5 mgSquare"><img src={plate5}/></div>
+    <div className="mgSquare6 mgSquare"><img src={plate2}/></div>
     </div>
 
     <button className="mgButton">Enter</button>
