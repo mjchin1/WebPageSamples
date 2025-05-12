d88ad40e4f3600f395b7cfa1a279bd21e21a2cce
Melissa J. Chin <melissa1jchin@gmail.com>
2025-05-12 12:38:39 -0500

    formatting shop page

--- client/src/Components/BigBookP2.jsx
+++ client/src/Components/BigBookP2.jsx
@@ -8,31 +8,62 @@ import {Link} from 'react-router-dom'
 
 export default function BigBook({}) {
 
+  const objects = [
+    {
+      productPhoto: sphereLamp,
+      productName: "Lamp"
+    },
+  ]
+
 
 
 
 
   return (
     <>
-
     <div className="bbkHeader">
     <h1 className="bbkBlackLogo">Big Book</h1>
+    <div className="bbkNav">
     <Link to = '/'><span>Objects</span></Link>
     <Link to = '/'><span>Lighting</span></Link>
     <Link to = '/'><span>Furniture</span></Link>
-    <Link to = '/'><h3 className="bbkNav">Home</h3></Link>
+    <Link to = '/'><span>Home</span></Link>
     </div>
+    </div> <br/><br/><br/> <br/> <br/> <br/>
 
-    <div className="bbkContainer"> 
-      <div className="objectsContainer">
-        <div className="shopObject">
-          <img src={sphereLamp}></img>
-          <span>Lamp</span>
+    <div className="bbkShopContainer"> 
+      
+      <h1 className="bbkShopContainerHeading"> Objects</h1>
+      <div className="bbkObjectContainer">
+        {objects.map((object)=> (
+        <div className="shopProduct">
+          <img className="shopProductPhoto" src={sphereLamp}></img>
+          <span className="shopProductTitle">{object.productName}</span>
         </div>
+         ))}
+      </div> <br/> <br/>
+      
+      <h1 className="bbkShopContainerHeading">Lighting</h1>
+      <div className="bbkLighingContainer">
+        {objects.map((object)=> (
+        <div className="shopProduct">
+          <img className="shopProductPhoto" src={sphereLamp}></img>
+          <span className="shopProductTitle">{object.productName}</span>
+        </div>
+         ))}
+      </div> <br/><br/>
+      
+      <h1 className="bbkShopContainerHeading">Furniture</h1>
+      <div className="bbkFurnitureContainer">
+        {objects.map((object)=> (
+        <div className="shopProduct">
+          <img className="shopProductPhoto" src={sphereLamp}></img>
+          <span className="shopProductTitle">{object.productName}</span>
+        </div>
+         ))}
       </div>
-      <div className="objectsContainer"></div>
-      <div className="objectsContainer"></div>
-    </div> 
+
+    </div>
   
     </>
 );
