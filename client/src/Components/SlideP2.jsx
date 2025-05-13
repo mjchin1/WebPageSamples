ff829eaec6cf16ad4ef0c1633fcc3b69a97d88d1
Melissa J. Chin <melissa1jchin@gmail.com>
2025-05-13 16:37:27 -0500

    adding photos to Slide shop

--- client/src/Components/SlideP2.jsx
+++ client/src/Components/SlideP2.jsx
@@ -1,11 +1,51 @@
 import greenTile from '../assets/Photos/Slide/greenTile.jpg'
+import greenTile2 from '../assets/Photos/Slide/greenTile2.jpg'
 import whiteTile from '../assets/Photos/Slide/whiteTile.jpg'
 import blueTile from '../assets/Photos/Slide/blueTile.jpg'
+import blueTile2 from '../assets/Photos/Slide/blueTile2.jpg'
+import blueTile3 from '../assets/Photos/Slide/blueTile3.jpg'
+import blueTile4 from '../assets/Photos/Slide/blueTile4.jpg'
 import { useNavigate } from 'react-router-dom'
 import {Link} from 'react-router-dom'
 
 
 export default function Slide() {
+
+  const tiles = [
+      
+       {
+        tilePhoto: blueTile3,
+        tileName: "Blue Tile"
+       },
+
+       {
+        tilePhoto: blueTile4,
+        tileName: "Blue Tile"
+       },
+
+       {
+        tilePhoto: blueTile2,
+        tileName: "Blue Tile"
+       },
+
+      {
+        tilePhoto: greenTile,
+        tileName: "Green Tile 1"
+       },
+      {
+        tilePhoto: greenTile2,
+        tileName: "Green Tile 2"
+      },
+       {
+        tilePhoto: whiteTile,
+        tileName: "Blue Tile"
+       },
+
+       {
+        tilePhoto: blueTile,
+        tileName: "Blue Tile"
+       },
+  ]
 
   const navigate = useNavigate()
 
@@ -28,10 +68,13 @@ export default function Slide() {
     </div> 
     
     <div className="sldShopContainer">
-      <div className="shopProduct"><img src={greenTile}/></div>
-      <div className="shopProduct"><img src={whiteTile}/></div>
-      <div className="shopProduct"><img src={blueTile}/></div>
-    </div>
+    {tiles.map((tile)=> (
+        <div className="shopProduct bbkProduct">
+          <img className="shopProductPhoto" src={tile.tilePhoto}></img>
+          <span className="shopProductTitle">{tile.tileName}</span>
+        </div>
+         ))}
+   </div>
     
 
     <div className="footer sldFooter"> © 2025 </div>
