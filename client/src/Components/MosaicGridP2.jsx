ac0677f2fc58f5f711fd7d974c971507f9d2c953
Melissa J. Chin <melissa1jchin@gmail.com>
2025-05-15 14:59:25 -0500

    formatting mosaic grid p 2

--- client/src/Components/MosaicGridP2.jsx
+++ client/src/Components/MosaicGridP2.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom'
+import tableSetting from '../assets/Photos/MosaicGrid/tableSetting.jpg'
+import pomegranate from '../assets/Photos/MosaicGrid/pomegranate.jpg'
 
 export default function MosaicGridP2() {
 
   return (
+    <>
 
-    <div className="mgHeader">
+    <div className="mgHeaderP2">
     <h1 className="mgLogo">Mosaic Grid</h1>
     <div className="mgNav"> 
     <Link to = '/'><h3 className="mgNavLink">Food</h3></Link>
@@ -13,5 +16,18 @@ export default function MosaicGridP2() {
     <Link to = '/'><h3 className="mgNavLink">Home</h3></Link>
     </div>
     </div>
+
+    <div className="mgp2Container">
+    <div className="mgp2Banner"> <h1 className="mgp2BannerText">Food</h1>
+    <img src={tableSetting}></img>
+    </div>  <br/>
+
+    <div className="mgp2ContentBlock1">
+    <div className= "mg2Block"></div>
+    <div className= "mg2Block"><img src={pomegranate}/></div>
+    </div>
+      
+    </div>
+    </>
   )
 }
