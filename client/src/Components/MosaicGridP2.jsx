1e87b2af4a4198c52d05e9cd0cae56a45f4514cf
Melissa J. Chin <melissa1jchin@gmail.com>
2025-05-15 16:05:27 -0500

    updating photos for mosaicGrid p2

--- client/src/Components/MosaicGridP2.jsx
+++ client/src/Components/MosaicGridP2.jsx
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom'
-import tableSetting from '../assets/Photos/MosaicGrid/tableSetting.jpg'
-import pomegranate from '../assets/Photos/MosaicGrid/pomegranate.jpg'
+import tableSetting3 from '../assets/Photos/MosaicGrid/tableSetting3.jpg'
+import plate5 from '../assets/Photos/MosaicGrid/plate5.jpg'
+import tableSetting5 from '../assets/Photos/MosaicGrid/tableSetting5.jpg'
+import tableSetting11 from '../assets/Photos/MosaicGrid/tableSetting11.jpg'
+import tableSetting7 from '../assets/Photos/MosaicGrid/tableSetting7.jpg'
+import oranges from '../assets/Photos/MosaicGrid/oranges.jpg'
+import tomatoes from '../assets/Photos/MosaicGrid/tomatoes.jpg'
+
 
 export default function MosaicGridP2() {
 
@@ -19,32 +25,32 @@ export default function MosaicGridP2() {
 
     <div className="mgp2Container">
     <div className="mgp2Banner"> <h1 className="mgp2BannerText">Food</h1>
-    <img src={tableSetting}></img>
+    <img src={tableSetting3}></img>
     </div>  <br/>
 
     <div className="mgp2ContentBlock1">
     <div className= "mg2Block"><h2>Our food is the best.</h2></div>
-    <div className= "mg2Block"><img src={pomegranate}/></div>
+    <div className= "mg2Block"><img src={plate5}/></div>
     </div>
     <h1>Gallery</h1>
     <div className="galleryContainer"></div>
     <div className="mgp2Banner"> <h1 className="mgp2BannerText">Gatherings</h1>
-    <img src={tableSetting}></img>
+    <img src={tableSetting5}></img>
     </div>  <br/>
 
     <div className="mgp2ContentBlock1">
     <div className= "mg2Block"><h2>We create experiences that you and your loved ones will remember forever.</h2></div>
-    <div className= "mg2Block"><img src={pomegranate}/></div>
+    <div className= "mg2Block"><img src={tableSetting11}/></div>
     </div>
     <h1>Gallery</h1>
     <div className="galleryContainer"></div>
     <div className="mgp2Banner"> <h1 className="mgp2BannerText">About Us</h1>
-    <img src={tableSetting}></img>
+    <img src={oranges}></img>
     </div>  <br/>
 
     <div className="mgp2ContentBlock1">
     <div className= "mg2Block"><h2>We put a ton of heart into everything we do.</h2></div>
-    <div className= "mg2Block"><img src={pomegranate}/></div>
+    <div className= "mg2Block"><img src={tomatoes}/></div>
     </div>
     <h1>Gallery</h1>
     <div className="galleryContainer"></div>
