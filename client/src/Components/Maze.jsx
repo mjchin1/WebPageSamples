9de2c9da5f0b28a08fb89b8e0420a665cd7187b5
Melissa J. Chin <melissa1jchin@gmail.com>
2025-08-11 16:16:11 -0500

    adding vertical and horizontal line divs

--- client/src/Components/Maze.jsx
+++ client/src/Components/Maze.jsx
@@ -8,7 +8,7 @@ export default function Maze() {
     <>
     <div className="mazeContainer"> 
 
-    <div className="mazeSquare1 mazeSquare">1</div>
+    {/* <div className="mazeSquare1 mazeSquare">1</div>
     <div className="mazeSquare2 mazeSquare">2</div>
     <div className="mazeSquare3 mazeSquare">3</div>
     <div className="mazeSquare4 mazeSquare">4</div> 
@@ -43,7 +43,33 @@ export default function Maze() {
     <div className="mazeSquare33 mazeSquare">33</div>
     <div className="mazeSquare34 mazeSquare">34</div>
     <div className="mazeSquare35 mazeSquare">35</div>
-    <div className="mazeSquare35 mazeSquare">36</div>
+    <div className="mazeSquare35 mazeSquare">36</div> */}
+
+    
+
+    <div className="verticalLine1"></div>
+    <div className="verticalLine2"></div>
+    <div className="verticalLine3"></div>
+    <div className="verticalLine3"></div>
+    <div className="verticalLine4"></div>
+    <div className="verticalLine5"></div>
+    <div className="verticalLine7"></div>
+    <div className="verticalLine8"></div>
+    <div className="verticalLine9"></div>
+    <div className="verticalLine10"></div>
+    
+    <div className="horizontalLine1"></div>
+    <div className="horizontalLine2"></div>
+    <div className="horizontalLine3"></div>
+    <div className="horizontalLine4"></div>
+    <div className="horizontalLine5"></div>
+    <div className="horizontalLine6"></div>
+    <div className="horizontalLine7"></div>
+    <div className="horizontalLine8"></div>
+    <div className="horizontalLine9"></div>
+    <div className="horizontalLine10"></div>
+    <div className="horizontalLine11"></div>
+
 
     <div className="mazeTitleSquare"> Melissa Chin</div>
 
