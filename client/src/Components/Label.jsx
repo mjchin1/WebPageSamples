5ed0bf588cf6c554dab8f78a798e659a75375867
Melissa J. Chin <melissa1jchin@gmail.com>
2025-09-03 18:17:57 -0500

    label design

--- client/src/Components/Label.jsx
+++ client/src/Components/Label.jsx
@@ -6,7 +6,14 @@ export default function Label() {
 
   return (
     <>
-    <div className="labelContainer"></div>
+    <div className="labelContainer">
+        <div className="lblTitleDiv">Label</div>
+        <div className="lblDiv1">Div1</div>
+        <div className="lblDiv2">Div2</div>
+        <div className="lblDiv3">Div3</div>
+        <div className="lblDiv4">Div4</div>
+    </div>
+
    
     </>
 );
