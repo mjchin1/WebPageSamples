2a4c52b140b7b09668ae424250f8c061b212d911
Melissa J. Chin <melissa1jchin@gmail.com>
2025-09-04 16:06:08 -0500

    adding label 2 template

---
+++ client/src/Components/Label2.jsx
@@ -0,0 +1,28 @@
+export default function Label2() {
+
+
+
+
+
+  return (
+    <>
+    <div className="label2Container">
+        <div className="lbl2TitleDiv">Label</div>
+        <div className="lbl2CenterDivs">
+        <div className="lbl2Div1">Div1</div>
+        <div className="lbl2DividingLine1"></div>
+        <div className="lbl2Div2">Div2</div>
+        <div className="lbl2DividingLine2"></div>
+        <div className="lbl2Div3">Div3</div>
+        </div>
+
+        <div className="lbl2Div4">Div4</div>
+    </div>
+
+   
+    </>
+);
+
+}
+
+
